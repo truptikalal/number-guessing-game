22c157077a5a94d2a484dd306e53705037ce277a
truptikalal <119340935+truptikalal@users.noreply.github.com>
2025-02-05 10:14:50 +0530

    Update and rename .gitignore to number.py

---
+++ number.py
@@ -0,0 +1,21 @@
+import random
+
+secret_no=random.randint(1,100)
+tries_left = 7
+
+while tries_left > 0:
+    try:
+        guess=int(input("guess the number between 1 to 100(You have {tries_left} tries left):"))
+        if guess == secret_no:
+            print("congras your guess is correct.")
+            break
+        elif guess < secret_no:
+            print("too low!")
+        else:
+            print("too high!")
+
+        tries_left -=1
+    except ValueError:
+        print("invalid input.please entr a number")
+if tries_left==0:
+    print(f"you ran out of triess. the no was {secret_no}.")
